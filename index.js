ba4107cdeeec61ee8d30aba82c20855744f6140e
GeorgeKryptonian <54601856+GeorgeKryptonian@users.noreply.github.com>
2021-04-08 14:08:44 +0300

    upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let num = 0;
 let c = document.querySelector('#clickBtn');
 let r = document.querySelector('#clickReload');
+let w = document.querySelector('#wrapper');
 function isClick() {
     c.textContent = `${++num}`;
     console.log(num);
     setTimeout(() => {
+        w.classList.add('wrapper-col');
         c.classList.add('click-btn-orange');
         c.textContent = `${num / 10} CPS`;
         c.removeAttribute('onclick', isClick);
