9ae3492acaa4e68e3909f04d730a766dc47ec650
GeorgeKryptonian <54601856+GeorgeKryptonian@users.noreply.github.com>
2021-04-08 13:19:25 +0300

    upload

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let num = 0;
-let c = document.querySelector('#click');
+let c = document.querySelector('#clickBtn');
+let r = document.querySelector('#clickReload');
 function isClick() {
     c.textContent = `${++num}`;
     console.log(num);
+    setTimeout(() => {
+        c.classList.add('click-btn-orange');
+        c.textContent = `${num / 10} CPS`;
+        c.removeAttribute('onclick', isClick);
+        r.hidden = false;
+    }, 10000);
+}
+function isReload() {
+    location.reload();
 }
